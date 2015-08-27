a75ef011c5897ebb3b4828d795a409daeaf0fd88
jleuschner <jens@marieswelt.com>
2015-08-27 07:18:16 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,154 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var PSCommandService = require('powershell-command-executor');
+var StatefulProcessCommandProxy = require('stateful-process-command-proxy');
+var CommandRegistry = require('./commandregistry');
+
+var CommandProxy = new StatefulProcessCommandProxy({
+  name: "CommandProxy",
+  max: 3,
+  min: 0,
+  idleTimeoutMS: 30000,
+  logFunction: function(severity,origin,msg) {
+    console.log(severity.toUpperCase() + " " +origin+" "+ msg);
+  },
+  processCommand: 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe',
+  processArgs:    ['-Command','-'],
+  processRetainMaxCmdHistory : 20,
+  processInvalidateOnRegex : {
+    'any':[],
+    'stdout':[],
+    'stderr':[{'regex':'.*error.*'}] // anything comes in on stderr, w/ "error"... invalidate it
+  },
+  processCwd : null,
+  processEnvMap : null,
+  processUid : null,
+  processGid : null,
+  initCommands : [ 'Import-Modul activedirectory' ],
+  /*  Array von Befehlen, die beim Start einer Shell ausgeführt werden, zB: Import-Module ...
+  initCommands: o365Utils.getO365PSInitCommands(
+                  PATH_TO_DECRYPT_UTILS_SCRIPT,
+                  PATH_TO_ENCRYPTED_CREDENTIALS_FILE,
+                  PATH_TO_SECRET_KEY,
+                  10000,30000,60000),
+	*/
+    validateFunction: function(processProxy) {
+      return processProxy.isValid();
+    },
+    /* Array von Befehlen, die vor dem Beenden der Shell ausgeführt werden
+	preDestroyCommands: o365Utils.getO365PSDestroyCommands(),
+    */
+    /* Intervallprüfung, ob Shell noch gültig
+	autoInvalidationConfig: o365Utils.getO365AutoInvalidationConfig(30000),
+	*/
+	/*
+	processCmdBlacklistRegex: o365Utils.getO365BlacklistedCommands(),
+    processCmdWhitelistRegex: o365Utils.getO365WhitelistedCommands()
+	*/
+  });
+
+var myLogFunction = function(severity,origin,message) {
+  console.log(severity.toUpperCase() + ' ' + origin + ' ' + message);
+}
+
+var CommandService = new PSCommandService(CommandProxy,CommandRegistry.Commands,myLogFunction);
+
+// GET command-service status
+app.get('/command-service/status', function(req, res, next) {
+    var status = CommandService.getStatus();
+    res.send(status);
+});
+
+// GET commands
+app.get('/command/:commandName?', function(req, res, next) {
+
+  if (req.params.commandName) {
+      res.send(CommandRegistry.Commands[req.params.commandName]);
+  } else {
+      res.send(CommandService.getAvailableCommands());
+  }
+});
+
+// POST (generate command statement)
+app.post('/command/generate/:commandName', function(req, res, next) {
+    var command = CommandService.generateCommand(req.params.commandName,req.body);
+    res.send({'command': command});
+});
+
+// POST (execute command)
+app.post('/command/execute/:commandName', function(req, res, next) {
+	console.log("BODY: ");
+	console.log(req.body);
+	CommandService.execute(req.params.commandName,req.body)
+      .then(function(cmdResult) {
+          res.send(cmdResult);
+      }).catch(function(error) {
+          res.send({'error': { 'name': error.name, 'message': error.message, 'stack':error.stack}});
+      });
+});
+
+
+/* GET - console page */
+app.get('/', function(req, res, next) {
+  res.sendFile('console.html', { root: './public'});
+});
+
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+		
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+		/*
+		res.send(err.message);
+		*/
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+//console.log(process.env);
+
+module.exports = app;
---
+++ commandregistry.js
@@ -0,0 +1,336 @@
+module.exports.getO365PSInitCommands = function(pathToDecryptUtilScript,
+                                                pathToCredsFile,
+                                                pathToKeyFile,
+                                                openTimeout,
+                                                operationTimeout,
+                                                idleTimeout) {
+  return [
+        // #0 Encoding UTF8
+        'chcp 65001',
+        '$OutputEncoding = [System.Text.Encoding]::GetEncoding(65001)',
+
+        // #1 import some basics
+        'Import-Module MSOnline',
+
+        // #2 source the decrypt utils script
+        // https://github.com/bitsofinfo/powershell-credential-encryption-tools/blob/master/decryptUtil.ps1
+        ('. ' + pathToDecryptUtilScript),
+
+        // #3 invoke decrypt2PSCredential to get the PSCredential object
+        // this function is provided by the sourced file above
+        ('$PSCredential = decrypt2PSCredential ' + pathToCredsFile + ' ' + pathToKeyFile),
+
+        // #4+ establish the session to o365
+        ('$sessionOpt = New-PSSessionOption -OpenTimeout '+openTimeout+' -OperationTimeout '+operationTimeout+' -IdleTimeout ' + idleTimeout),
+        '$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $PSCredential -Authentication Basic -AllowRedirection -SessionOption $sessionOpt',
+
+        // #5 import the relevant cmdlets (TODO: make this configurable)
+        'Import-PSSession $session -CommandName *DistributionGroup* -AllowClobber',
+        'Import-PSSession $session -CommandName *Contact* -AllowClobber',
+
+        // #6 connect to azure as well
+        'Connect-MsolService -Credential $PSCredential',
+
+        // #7 cleanup
+        'Remove-Variable -Force -ErrorAction SilentlyContinue $PSCredential'
+  ]
+}
+
+/**
+* Destroy commands that correspond to the session
+* established w/ the initCommands above
+*/
+module.exports.getO365PSDestroyCommands = function() {
+    return [
+          'Get-PSSession | Remove-PSSession',
+          'Remove-PSSession -Session $session',
+          'Remove-Module MsOnline'
+          ]
+  }
+
+/**
+* Some example blacklisted commands
+*/
+module.exports.getO365BlacklistedCommands = function() {
+  return [
+      {'regex':'.*Invoke-Expression.*', 'flags':'i'},
+      {'regex':'.*ScriptBlock.*', 'flags':'i'},
+      {'regex':'.*Get-Acl.*', 'flags':'i'},
+      {'regex':'.*Set-Acl.*', 'flags':'i'},
+      {'regex':'.*Get-Content.*', 'flags':'i'},
+      {'regex':'.*-History.*', 'flags':'i'},
+      {'regex':'.*Out-File.*', 'flags':'i'}
+  ]
+}
+
+/**
+* Configuration auto invalidation, checking PSSession availability
+* @param checkIntervalMS
+*/
+module.exports.getO365AutoInvalidationConfig = function(checkIntervalMS) {
+      return {
+              'checkIntervalMS': checkIntervalMS,
+              'commands': [
+              // no remote pssession established? invalid!
+              { 'command': 'Get-PSSession',
+                'regexes': {
+                  'stdout' : [ {'regex':'.*Opened.*', 'flags':'i', 'invalidOn':'noMatch'}]
+                }
+              }]
+          };
+  }
+
+
+/**
+* Defines a registry of Powershell commands
+* that can be injected into the PSCommandService
+* instance.
+*
+* Note these are just some example configurations specifically for a few
+* o365 functions and limited arguments for each, (they work) however you may want to
+* replace this with your own set of init command tailored to your specific
+* use-case
+*/
+var o365CommandRegistry = {
+
+    /*******************************
+    *
+    * o365 Powershell Command registry
+    *
+    * argument properties (optional):
+    *    - quoted: true|false, default true
+    *    - valued: true|false, default true
+    *    - default: optional default value (only if valued..)
+    *
+    * return properties:
+    *   type: none, text or json are valid values
+    *
+    ********************************/
+
+    /*******************************
+    * MsolUser
+    ********************************/
+
+    'Resolve-DnsName': {
+      'command': 'Resolve-DnsName {{{arguments}}} | ConvertTo-Json -Compress',
+      'arguments': {
+        'name': {}
+      },
+      'return': { type: 'json' }
+    },
+
+
+    'getMsolUser': {
+      'command': 'Get-MsolUser {{{arguments}}} | ConvertTo-Json',
+      'arguments': {
+        'UserPrincipalName': {}
+      },
+      'return': { type: 'json' }
+    },
+
+    'newMsolUser': {
+      'command': 'New-MsolUser {{{arguments}}} | ConvertTo-Json',
+      'arguments': {
+        'DisplayName': {},
+        'UserPrincipalName': {}
+      },
+      'return': { type: 'json' }
+    },
+
+    'removeMsolUser': {
+      'command': 'Remove-MsolUser -Force {{{arguments}}} ',
+      'arguments': {
+        'UserPrincipalName': {}
+      },
+      'return': { type: 'none' }
+    },
+
+    /*******************************
+    * DistributionGroups
+    ********************************/
+
+    'getDistributionGroup': {
+        'command': 'Get-DistributionGroup {{{arguments}}} | ConvertTo-Json',
+        'arguments': {
+            'Identity': {}
+        },
+        'return': { type: 'json' }
+    },
+
+    'newDistributionGroup': {
+
+        'command': 'New-DistributionGroup -Confirm:$False {{{arguments}}} | ConvertTo-Json',
+
+        'arguments': {
+            'Name':               {},
+            'DisplayName':        {},
+            'Alias':              {},
+            'PrimarySmtpAddress': {},
+            'Type':               {'quoted':false, 'default':'Security'},
+            'ManagedBy':          {},
+            'Members':            {}, // specifying members on create does not seem to work
+            'ModerationEnabled':              { 'default':'0', 'quoted':false},
+            'MemberDepartRestriction':        { 'default':'Closed'},
+            'MemberJoinRestriction':          { 'default':'Closed'},
+            'SendModerationNotifications':    { 'default':'Never', 'quoted':false},
+
+        },
+        'return': { type: 'json' }
+    },
+
+    'setDistributionGroup': {
+
+        'command': 'Set-DistributionGroup -Confirm:$False {{{arguments}}}',
+
+        'arguments': {
+            'Identity':           {},
+            'Name':               {},
+            'DisplayName':        {},
+            'Alias':              {},
+            'PrimarySmtpAddress': {},
+            'ManagedBy':          {},
+            'Members':            {},
+            'MailTip':            {},
+            'ModerationEnabled':              { 'default':'0', 'quoted':false},
+            'MemberDepartRestriction':        { 'default':'Closed'},
+            'MemberJoinRestriction':          { 'default':'Closed'},
+            'SendModerationNotifications':    { 'default':'Never', 'quoted':false},
+            'BypassSecurityGroupManagerCheck': {'valued': false}
+        },
+        'return': { type: 'none' }
+    },
+
+
+    'removeDistributionGroup': {
+
+        'command': 'Remove-DistributionGroup {{{arguments}}} -Confirm:$false',
+
+        'arguments': {
+            'Identity':           {},
+            // needed if invoking as global admin who is not explicitly a group admin.. stupid... yes.
+            'BypassSecurityGroupManagerCheck': {'valued': false}
+        },
+        'return': { type: 'none' }
+    },
+
+
+    'getDistributionGroupMember': {
+
+        'command': 'Get-DistributionGroupMember {{{arguments}}} | ConvertTo-Json',
+
+        'arguments': {
+            'Identity':           {}
+        },
+        'return': { type: 'json' }
+    },
+
+
+    'addDistributionGroupMember': {
+
+        'command': 'Add-DistributionGroupMember {{{arguments}}}',
+
+        'arguments': {
+            'Identity':           {},
+            'Member':             {},
+            // needed if invoking as global admin who is not explicitly a group admin.. stupid... yes.
+            'BypassSecurityGroupManagerCheck': {'valued': false}
+        },
+        'return': { type: 'none' }
+    },
+
+    // members specified w/ this are a full overwrite..
+    'updateDistributionGroupMembers': {
+
+        'command': 'Update-DistributionGroupMember -Confirm:$false {{{arguments}}}',
+
+        'arguments': {
+            'Identity':           {},
+            'Members':            {},
+            // needed if invoking as global admin who is not explicitly a group admin.. stupid... yes.
+            'BypassSecurityGroupManagerCheck': {'valued': false}
+        },
+        'return': { type: 'none' }
+    },
+
+    'removeDistributionGroupMember': {
+
+        'command': 'Remove-DistributionGroupMember {{{arguments}}} -Confirm:$false',
+
+        'arguments': {
+            'Identity':          {},
+            'Member':            {},
+            // needed if invoking as global admin who is not explicitly a group admin.. stupid... yes.
+            'BypassSecurityGroupManagerCheck': {'valued': false}
+        },
+        'return': { type: 'none' }
+    },
+
+
+
+
+    /*******************************
+    * MailContacts
+    ********************************/
+
+    'getMailContact': {
+        'command': 'Get-MailContact {{{arguments}}} | ConvertTo-Json',
+        'arguments': {
+            'Identity': {}
+        },
+        'return': { type: 'json' }
+    },
+
+    'newMailContact': {
+
+        'command': 'New-MailContact -Confirm:$False {{{arguments}}} | ConvertTo-Json',
+
+        'arguments': {
+            'Name':                  {},
+            'ExternalEmailAddress':  {}
+        },
+
+        'return': { type: 'json' }
+    },
+
+    'setMailContact': {
+
+        'command': 'Set-MailContact -Confirm:$False {{{arguments}}}',
+
+        'arguments': {
+            'Identity':             {},
+            'Name':                 {},
+            'DisplayName':          {},
+            'ExternalEmailAddress': {}
+        },
+
+        'return': { type: 'none' }
+    },
+
+
+    'removeMailContact': {
+
+        'command': 'Remove-MailContact {{{arguments}}} -Confirm:$false',
+
+        'arguments': {
+            'Identity':           {}
+        },
+
+        'return': { type: 'none' }
+    }
+};
+
+module.exports.Commands = o365CommandRegistry;
+
+/**
+* Some example whitelisted commands
+* (only permit) what is in the registry
+*/
+module.exports.getO365WhitelistedCommands = function() {
+    var whitelist = [];
+    for (var cmdName in o365CommandRegistry) {
+        var config = o365CommandRegistry[cmdName];
+        var commandStart = config.command.substring(0,config.command.indexOf(' ')).trim();
+        whitelist.push({'regex':'^'+commandStart+'\\s+.*', 'flags':'i'});
+    }
+    return whitelist;
+}
